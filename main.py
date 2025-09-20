5e10dcac33cf33a7c34a22e3814be4b2eaf4a513
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-09-20 20:13:18 +0300

    Added config file reading.

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import yaml
+
+
+class Config:
+    def __init__(self, file_dir):
+        self.file_dir = file_dir
+        self._data = self._load_config()
+
+    def _load_config(self):
+        with open(self.file_dir, 'r') as f:
+            return yaml.safe_load(f)
+
+    def get_data(self):
+        return self._data
+
+class Processor:
+    def __init__(self, config_data):
+        self.config_data = config_data
+
+    def load_data(self):
+        for market_name, market_info in self.config_data.items():
+            print(market_info)
+
 def main():
-    print("Hello World")
+    config = Config("./config.yaml")
+    processor = Processor(config.get_data())
+    processor.load_data()
 
 if __name__ == "__main__":
     main()
