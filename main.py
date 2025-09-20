8825d50d01aa4776ba1871b5a25a084954630733
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-09-21 01:51:27 +0300

    Added loading of the data in the dataframe.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yaml
+import pandas as pd
 from pypxlib import Table
 
 
@@ -17,18 +18,38 @@ class Config:
 class Processor:
     def __init__(self, config_data):
         self.config_data = config_data
+        self.dataset = pd.DataFrame(
+            columns = [
+                'settlement',
+                'market_name',
+                'item_name',
+                'item_code',
+                'item_kzp_category_code',
+                'item_retail_price',
+                'item_promotional_price'
+            ]
+        )
 
     def load_data(self):
         for market_name, market_info in self.config_data.items():
             print(market_info)
-            table = Table(market_info["path_to_db"], encoding='windows-1251')
+            table = Table(market_info['path_to_db'], encoding='windows-1251')
             for row in table:
-                print(row)
+                self.dataset.loc[len(self.dataset)] = [
+                    market_info['settlement'],
+                    market_info['name'],
+                    row.Item,
+                    row.id,
+                    "Not Yet Calculated!!!",
+                    row.ClientPrice,
+                    0
+                ]
+            print(self.dataset.head())
 
 def main():
-    config = Config("./config.yaml")
+    config = Config('./config.yaml')
     processor = Processor(config.get_data())
     processor.load_data()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
